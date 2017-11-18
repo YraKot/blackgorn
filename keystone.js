ae17cd3f40da3e687c510640883a364442471f54
YraKot <y.kitsera@gmail.com>
2017-11-18 19:11:43 +0200

    add footer

--- keystone.js
+++ keystone.js
@@ -15,7 +15,7 @@ keystone.init({
 
 	'stylus': 'public',
 	'static': 'public',
-	'favicon': 'public/favicon.ico',
+	'favicon': 'public/favcon.ico',
 	'views': 'templates/views',
 	'view engine': 'pug',
 
